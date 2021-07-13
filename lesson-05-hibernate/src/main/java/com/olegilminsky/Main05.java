e45110673fb9f3ffddda0bcc45195db5ebba6ebc
olegilminsky <olegilminsky@yandex.ru>
2021-07-13 15:09:10 +0300

    Java Level 5 (Spring Framework) started group 2021.06.15 Added path classwork 5 path 02 (Hibernate).

--- lesson-05-hibernate/src/main/java/com/olegilminsky/Main05.java
+++ lesson-05-hibernate/src/main/java/com/olegilminsky/Main05.java
@@ -5,6 +5,7 @@ import org.hibernate.cfg.Configuration;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import java.util.List;
 
 public class Main05 {
     public static void main(String[] args) {
@@ -18,10 +19,24 @@ public class Main05 {
 
         em.getTransaction().begin();
 
-        User user = new User(null, "user1", 25);
-        em.persist(user);
+        List.of(
+                new User(null, "user2", 18),
+                new User(null, "alex", 24),
+                new User(null, "ivan", 57),
+                new User(null, "petr", 34),
+                new User(null, "julia", 27),
+                new User(null, "max", 33)
+        ).forEach(em::persist);
 
         em.getTransaction().commit();
+
+        // SELECT
+
+        User user = em.find(User.class, 1L);
+
+        System.out.println(user);
+
+
 
         em.close();
     }
